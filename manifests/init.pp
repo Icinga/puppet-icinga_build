c7605f72dae9f1cf5cb80835ce07f132ee228255
Jean Flach <jean-marcel.flach@netways.de>
2017-01-31 11:02:26 +0100

    Add build scripts

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,8 @@ class icinga_build {
   contain ::jenkins
   contain ::jenkins::cli_helper
 
+  contain ::icinga_build::scripts
+
   create_resources('icinga_build::job', hiera_hash('jenkins::job', {}))
   create_resources('jenkins::plugin', hiera_hash('jenkins::plugin', {}))
 }
---
+++ manifests/scripts.pp
@@ -0,0 +1,12 @@
+class icinga_build::scripts {
+  file { '/var/lib/jenkins/jenkins-scripts':
+    ensure => 'directory', 
+    owner => 'jenkins' ,
+    group => 'jenkins',
+    mode  => '755',
+    recurse => true,
+    force => true,
+    purge => true,
+    source => 'puppet:///modules/icinga_build/jenkins-scripts'
+  }
+}
