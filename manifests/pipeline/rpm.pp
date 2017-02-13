f2d60613262da22c24b2d80d464c4c5aebb6d5ae
Markus Frosch <markus@lazyfrosch.de>
2017-02-13 09:11:02 +0100

    pipeline: Add rpm builds

---
+++ manifests/pipeline/rpm.pp
@@ -0,0 +1,48 @@
+define icinga_build::pipeline::rpm (
+  $pipeline,
+  $product,
+  $control_repo,
+  $control_branch,
+  $os            = undef, # part of namevar
+  $dist          = undef, # part of namevar
+  $arch          = $icinga_build::pipeline::defaults::arch,
+  $docker_image  = $icinga_build::pipeline::defaults::docker_image,
+  $jenkins_label = $icinga_build::pipeline::defaults::jenkins_label,
+) {
+  validate_array($arch)
+  validate_string($docker_image, $jenkins_label)
+
+  unless $arch and $docker_image and $jenkins_label {
+    fail('Please ensure to configure icinga_build::pipeline::defaults, or add the parameters directly')
+  }
+
+  if $os and $dist {
+    $_os = $os
+    $_dist = $dist
+  } elsif $name =~ /-([\w\d\._]+)-([\w\d\._]+)$/ {
+    $_os = $1
+    $_dist = $2
+  } else {
+    fail("Can not parse os/dist from name: ${name}")
+  }
+
+  validate_string($_os, $_dist)
+
+  $_docker_image = regsubst(regsubst($docker_image, '{os}', $_os), '{dist}', $_dist)
+  $_docker_image_source = regsubst($_docker_image, '{arch}', $arch[0])
+  $_docker_image_binary = regsubst($_docker_image, '{arch}', '$arch')
+
+  $_source_job = "rpm-${_os}-${_dist}-0source"
+  $_binary_job = "rpm-${_os}-${_dist}-1binary"
+
+  jenkins_job { "${pipeline}/${_source_job}":
+    config => template('icinga_build/jobs/rpm_source.xml.erb'),
+  }
+
+  jenkins_job { "${pipeline}/${_binary_job}":
+    config => template('icinga_build/jobs/rpm_binary_matrix.xml.erb'),
+  }
+
+  # TODO: test
+  # TODO: release
+}
---
+++ spec/defines/pipeline/rpm_spec.rb
@@ -0,0 +1,72 @@
+require 'spec_helper'
+
+describe 'icinga_build::pipeline::rpm' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let :facts do
+        facts
+      end
+
+      context 'with a simple example' do
+        let :title do
+          'icinga2-snapshot-centos-7'
+        end
+
+        let :pre_condition do
+          "
+          class { 'icinga_build::pipeline::defaults':
+            arch          => ['x86_64', 'x86'],
+            jenkins_label => 'docker-test',
+            docker_image  => 'private-registry:5000/icinga/{os}-{dist}-{arch}',
+          }
+          "
+        end
+
+        let :params do
+          {
+            product:        'icinga2',
+            pipeline:       'icinga2-snapshot',
+            control_repo:   'https://github.com/Icinga/icinga-packaging.git',
+            control_branch: 'snapshot'
+          }
+        end
+
+        it { should compile.with_all_deps }
+
+        # pre_condition
+        it { should contain_class('icinga_build::pipeline::defaults') }
+
+        it { should contain_icinga_build__pipeline__rpm('icinga2-snapshot-centos-7') }
+
+        it do
+          should contain_jenkins_job('icinga2-snapshot/rpm-centos-7-0source')
+            .with_config(/#{Regexp.escape(params[:control_repo])}/)
+            .with_config(%r{\*/rpm/#{Regexp.escape(params[:control_branch])}})
+            .with_config(%r{<assignedNode>docker-test</assignedNode>})
+            .with_config(%r{<image>private-registry:5000/icinga/centos-7-x86_64</image>})
+            .with_config(%r{<includedRegions>icinga2/\*</includedRegions>})
+            .with_config(/project="icinga2"/)
+            .with_config(/os="centos"/)
+            .with_config(/dist="7"/)
+            .with_config(%r{rpmbuild --nodeps -bs})
+        end
+
+        it do
+          should contain_jenkins_job('icinga2-snapshot/rpm-centos-7-1binary')
+            .with_config(/matrix-project/)
+            .with_config(%r{<assignedNode>docker-test</assignedNode>})
+            .with_config(%r{<image>private-registry:5000/icinga/centos-7-\$arch</image>})
+            .with_config(%r{<projectNameList>\s*<string>rpm-centos-7-0source</string>\s*</projectNameList>}m)
+            .with_config(%r{<upstreamProjects>rpm-centos-7-0source</upstreamProjects>})
+            .with_config(%r{<hudson.matrix.TextAxis>\s*<name>arch</name>\s*<values>\s*<string>x86_64</string>\s*<string>x86</string>\s*</values>\s*</hudson.matrix.TextAxis>}m)
+            .with_config(%r{<project>rpm-centos-7-0source</project>}) # copy artifacts from
+            .with_config(/project="icinga2"/)
+            .with_config(/os="centos"/)
+            .with_config(/dist="7"/)
+            .without_config(/^arch=/)
+            .with_config(/rpmbuild --rebuild/)
+        end
+      end
+    end
+  end
+end
