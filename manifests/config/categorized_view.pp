1b3ddef3a886e20acde67fecb082ad33c73f703f
Markus Frosch <markus@lazyfrosch.de>
2017-03-01 10:39:09 +0100

    Remove miscommited file
    from f0423c6e69734bc276139d5f39a93a4af981bf28

--- manifests/config/categorized_view.pp
+++
@@ -1,9 +0,0 @@
-define icinga_build::config::categorized_view(
-  $name,
-  $description = '',
-  $columns = ['Status', 'org.foo.irgendwas'],
-) {
-  concat::fragment { 
-
-define icinga_build::config::view(
-  $regex = ".*"kkj
