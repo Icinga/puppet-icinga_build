3d0e84e7aa01686c18d02eb7c0d6e72667d16418
Markus Frosch <markus@lazyfrosch.de>
2017-04-20 11:46:32 +0200

    docker/debian: Update script
    Cleanup and fix Debian install (keyring)

--- files/jenkins-scripts/docker/debian.sh
+++ files/jenkins-scripts/docker/debian.sh
@@ -1,37 +1,48 @@
-#!/bin/bash -xe
+#!/bin/bash
 
-image_name=netways/$os-$release-$arch
+set -ex
 
-apt-get update
-apt-get install -y debootstrap gcc
+: ${os:=}
+: ${release:=}
+: ${arch:=}
+: ${IMAGE_PREFIX:='netways/'}
 
-keyring_arg=""
+if [ -z "$os" ]; then
+  echo "env variable 'os' not set!" >&2
+  exit 1
+elif [ -z "$release" ]; then
+  echo "env variable 'release' not set!" >&2
+  exit 1
+elif [ -z "$arch" ]; then
+  echo "env variable 'arch' not set!" >&2
+  exit 1
+fi
+
+image_name="${IMAGE_PREFIX}${os}-${release}-${arch}"
+
+keyring=
+keyring_arg=
+debootstrap_cmd=debootstrap
 
 if [ "$arch" = "x86" ] ; then
   link_arch="i386"
+elif [ "$arch" = "x86_64" ] ; then
+  link_arch="amd64"
 else
   link_arch="$arch"
-fi
-
-if [[ "$arch" == "arm*" ]] ; then
-  debootstrap_cmd=qemu-debootstrap
-
-  if [ "$release" = "squeeze" ]; then
-    echo "armhf/armel is not supported for Debian squeeze"
-    exit 0
-  fi
-
-  apt-get install -y qemu-user-static
 fi
 
 case $os in
 debian)
   mirror="http://mirror.noris.net/debian/"
   components="main"
+  keyring=debian-archive-keyring
+  keyring_arg="--keyring /usr/share/keyrings/debian-archive-keyring.gpg"
   ;;
 ubuntu)
   mirror="http://mirror.noris.net/ubuntu/"
   components="main,multiverse,universe"
+  keyring=ubuntu-keyring
   ;;
 raspbian)
   mirror="http://archive.raspbian.org/raspbian"
@@ -42,9 +53,14 @@ raspbian)
   ;;
 esac
 
-debootstrap_cmd=debootstrap
+apt-get update
+apt-get install -y debootstrap ${keyring}
 
 if [[ "$arch" == "arm*" ]] ; then
+  # TODO: replace this
+  echo "ARM disabled for now!" >&2
+  exit 1
+
   debootstrap_cmd=qemu-debootstrap
 
   if [ "$release" = "squeeze" ]; then
@@ -57,18 +73,17 @@ fi
 
 destdir=`mktemp -d`
 mount -t tmpfs none $destdir
-$debootstrap_cmd $keyring_arg --verbose --components=$components --include=debhelper --arch=$arch $release $destdir $mirror
+${debootstrap_cmd} ${keyring_arg} --verbose --components=$components --include=debhelper --arch=${link_arch} ${release} ${destdir} ${mirror}
 
 if [ "$os" = "debian" ]; then
-  cat > $destdir/etc/apt/sources.list <<APT
+  cat > "$destdir"/etc/apt/sources.list <<APT
 # $release
 deb     http://mirror.noris.net/debian $release          main contrib non-free
 deb-src http://mirror.noris.net/debian $release          main contrib non-free
-
 APT
 
   if [ "$release" != "unstable" ]; then
-    cat >> $destdir/etc/apt/sources.list <<APT
+    cat >> "$destdir"/etc/apt/sources.list <<APT
 # $release-security
 deb     http://security.debian.org/  $release/updates  main contrib non-free
 deb-src http://security.debian.org/  $release/updates  main contrib non-free
@@ -77,30 +92,35 @@ APT
 fi
 
 if [ "$os" = "raspbian" ]; then
-  cat > $destdir/etc/apt/sources.list <<APT
+  cat > "$destdir"/etc/apt/sources.list <<APT
 deb http://mirrordirector.raspbian.org/raspbian $release main firmware
 deb http://archive.raspberrypi.org/debian $release main
 APT
 
-  chroot $destdir apt-key adv --keyserver keyserver.ubuntu.com --recv-key 82B129927FA3303E
+  chroot "$destdir" apt-key adv --keyserver keyserver.ubuntu.com --recv-key 82B129927FA3303E
 fi
 
-chroot $destdir apt-get update
-chroot $destdir apt-get install --no-install-recommends -y --force-yes ccache build-essential:native bison ssh-client cmake flex g++ libboost-dev libboost-program-options-dev libboost-regex-dev libboost-system-dev libboost-test-dev libboost-thread-dev libmysqlclient-dev libpq-dev libssl-dev libyajl-dev fakeroot git devscripts sudo curl python wget pkg-config `cat jenkins-scripts/modules/extra-debian-packages`
+chroot ${destdir} apt-get update
+chroot ${destdir} apt-get install \
+  --no-install-recommends -y --force-yes \
+  ccache build-essential:native bison ssh-client cmake flex g++ libboost-dev \
+  libboost-program-options-dev libboost-regex-dev libboost-system-dev libboost-test-dev \
+  libboost-thread-dev libmysqlclient-dev libpq-dev libssl-dev libyajl-dev fakeroot git \
+  devscripts sudo curl python wget pkg-config
 
 if [ "$release" = "xenial" ]; then
-  chroot $destdir apt-get install -y libwxgtk3.0-dev
+  # TODO: why?
+  chroot ${destdir} apt-get install -y libwxgtk3.0-dev
 fi
 
-gcc --static -o $destdir/usr/bin/root_exec jenkins-scripts/modules/root_exec.c
-chroot $destdir chmod ug+s /usr/bin/root_exec
-
+# Add jenkins user
 chroot $destdir groupadd -g 1000 jenkins
 chroot $destdir useradd -u 1000 -g 1000 -m jenkins
 echo 'jenkins ALL=(ALL:ALL) NOPASSWD: ALL' | chroot $destdir tee -a /etc/sudoers
 
-tar c -C $destdir --one-file-system . | docker import - $image_name
+tar c -C ${destdir} --one-file-system . | docker import - ${image_name}
 
-umount -R $destdir
+umount -R ${destdir}
 
-source jenkins-scripts/docker/push_docker_image.sh
+export image_name
+./jenkins-scripts/docker/push_docker_image.sh
