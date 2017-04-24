24201acae927ddf6c091059161ee13c73e4231e9
Markus Frosch <markus@lazyfrosch.de>
2017-04-24 18:15:01 +0200

    docker/centos|suse: Add build_icing_org macro
    We need this for SPEC internal build dependencies against the
    packages.icinga.com repository.

--- files/jenkins-scripts/docker/suse.sh
+++ files/jenkins-scripts/docker/suse.sh
@@ -200,6 +200,9 @@ chroot "$destdir" groupadd -g 1000 jenkins
 chroot "$destdir" useradd -u 1000 -g 1000 -m jenkins
 echo 'jenkins ALL=(ALL:ALL) NOPASSWD: ALL' | chroot "$destdir" tee -a /etc/sudoers
 
+# TODO: Remove this once the build deps for boost have been cleaned up...
+echo '%build_icinga_org 1' >/etc/rpm/macros.icinga_build
+
 tar c -C "$destdir" --one-file-system . | docker import - "$image_name"
 
 umount -R "$destdir"
